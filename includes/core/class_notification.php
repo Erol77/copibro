f1df7240521170f3b1ed684f4309d3dfe2260cfa
Erol <veliev77@mail.ru>
2021-12-27 07:19:46 +0300

    added metod get user & notification

--- includes/core/class_notification.php
+++ includes/core/class_notification.php
@@ -5,5 +5,24 @@ class Notification {
     // TEST
 
     // your code here ...
+    public static function get(){
+        $user_id = $_GET['user_id'];
+        if ($user_id) {
+           $get_content = DB::query("SELECT title, description, created, viewed FROM user_notifications WHERE user_id='".$user_id."';") or die (DB::error());
+        } 
+        return $get_content ;
+    }
+
+
+    
+
+    public static function read(){        
+        $user_id = $_POST['user_id'];
+         if ($user_id) {
+            $result = DB::query("SELECT * FROM user_notifications  WHERE user_id='".$user_id."';") or die (DB::error());
+            $read_content =  json_encode($result->fetch_assoc(),JSON_HEX_QUOT);
+        } 
+        return $read_content; 
+    }
 
 }
--- includes/core/class_user.php
+++ includes/core/class_user.php
@@ -58,10 +58,25 @@ class User {
 
     public static function owner_info() {
         // your code here ...
+        $user_id = $_GET['user_id'];//получаем из гет параметра ид пользователя
+         if ($user_id) {
+            $result = DB::query("SELECT * FROM users WHERE user_id='".$user_id."' LIMIT 1;") or die (DB::error());// запрос на выборку пользователя из бд
+            $get =  json_encode($result->fetch_assoc(),JSON_HEX_QUOT);// получаем данные пользователя в json
+        } else {
+            $get = 'there is no data for the request'
+        }
+        return $get;        
     }
 
     public static function owner_update($data = []) {
         // your code here ...
+        if ( isset($data['last_name']) || isset($data['middle_name']) || isset($data['email']) || isset($data['phone'])){
+            return response('error_code'=>'there are no fields to change');
+        } else {
+            $
+        }
+        `first_name`, `last_name`, `middle_name`, `email`, `phone`
+        DB::query("UPDATE users SET login_attempts='0' WHERE user_id='".$user_id."' LIMIT 1;") or die (DB::error());
     }
 
 }
